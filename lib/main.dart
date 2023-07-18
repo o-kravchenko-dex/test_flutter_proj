609c17e62668c4937fef1985c47a5ace4f37f04c
o.kravchenko <o.kravchenko@dextechnology.com>
2023-07-18 10:24:45 +0300

    added: interactive view

--- lib/main.dart
+++ lib/main.dart
@@ -30,29 +30,13 @@ class _RandomWordsState extends State<RandomWords> {
   Widget build(BuildContext context) {
     return ColoredBox(
       color: Colors.blueGrey,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          AnimatedSwitcher(
-            duration: const Duration(milliseconds: 1000),
-            child: Text('$_count',
-                key: ValueKey<int>(_count),
-                style: Theme.of(context).textTheme.displayMedium),
-            transitionBuilder: (child, duration) => ScaleTransition(
-              scale: duration,
-              child: child,
-            ),
-          ),
-          ElevatedButton(
-            child: const Text("increment"),
-            onPressed: () {
-              setState(() {
-                _count += 1;
-              });
-            },
-          ),
-        ],
-      ),
-    );
+      child:InteractiveViewer(
+        child: Column(
+          children: [
+            Image.network('https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80', height: 500,),
+            OutlinedButton(onPressed: () {}, child: const Text("hellot"))
+          ],
+        ),
+      ));
   }
 }
